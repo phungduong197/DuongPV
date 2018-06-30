6a85f78fc8b9f5cbe76063493674dd9f70d9f74f
phungduong197 <phungduong197@gmail.com>
2018-06-30 11:36:44 +0700

    page static

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/contract', to: 'static_pages#contract'
+  get '/help', to: 'static_pages#help'
+  get  '/signup',  to: 'users#new'
+  resources :microposts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
