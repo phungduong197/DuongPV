32a0370bf8506468ac00e1c4e908348a9e97ec7b
phungduong197 <phungduong197@gmail.com>
2018-07-05 11:18:10 +0700

    Basic login

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby "2.5.1"
 gem "bootsnap", ">= 1.1.0", require: false
 gem "coffee-rails", "~> 4.2"
 gem "jbuilder", "~> 2.5"
+gem "jquery-rails"
 gem "puma", "~> 3.11"
 gem "rails", "~> 5.2.0"
 gem "sass-rails", "~> 5.0"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,9 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by id: params[:id]
-    unless @user
-      flash[:success] = t "user.show.err"
-      redirect_to root_url
-    end
+    return if @user
+    flash[:success] = t "user.show.err"
+    redirect_to root_url
   end
 
   def new
@@ -14,6 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
+      log_in @user
       flash[:success] = t "user.create.success"
       redirect_to @user
     else
