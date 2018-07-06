8055b4609ae01552edbd4c87840cdf565f1e6175
phungduong197 <phungduong197@gmail.com>
2018-07-06 13:34:17 +0700

    updating

--- Gemfile
+++ Gemfile
@@ -34,5 +34,8 @@ end
 
 gem "bcrypt", "3.1.12"
 gem "bootstrap-sass", "3.3.7"
+gem "bootstrap-will_paginate", "1.0.0"
 gem "config"
+gem "faker", "1.7.3"
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem "will_paginate", "3.1.6"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, except: [:index, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  before_action :find_id, only: [:show, :update, :destroy]
+  
+  def index
+    @users = User.paginate page: params[:page], per_page: Settings.limit
+  end
+
   def show
-    @user = User.find_by id: params[:id]
     return if @user
-    flash[:success] = t "user.show.err"
+    flash[:success] = t ".err"
     redirect_to root_url
   end
 
@@ -14,10 +22,29 @@ class UsersController < ApplicationController
     @user = User.new user_params
     if @user.save
       log_in @user
-      flash[:success] = t "user.create.success"
+      flash[:success] = t ".success"
       redirect_to @user
     else
       render :new
+    end
+  end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t ".success"
+      redirect_to users_url
+    else
+      flash[:danger] = t ".fail"
+      redirect_to users_url
     end
   end
 
@@ -26,5 +53,28 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t ".ple"
+    redirect_to login_url
+  end
+
+  def correct_user
+    @user = User.find_by id: params[:id]
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
+
+  def find_id
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t ".fail"
+    redirect_to home_url
   end
 end
