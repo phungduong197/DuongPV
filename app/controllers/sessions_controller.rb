b58755f3a067eabec7ed96d724426faa387793c8
phungduong197 <phungduong197@gmail.com>
2018-07-06 11:21:26 +0700

    adv login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,16 @@ class SessionsController < ApplicationController
     user = User.find_by email: params[:session][:email].downcase
     if user&.authenticate params[:session][:password]
       log_in user
+      check_remember user
       redirect_to user
     else
-      flash.now[:lazy] = t "sessions.new.daner"
+      flash.now[:danger] = t ".danger"
       render :new
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
